82e87c5c63c73028a2d09d3ac196b4c88d16a949
eftychis <eftychis@users.noreply.github.com>
2019-06-29 12:26:24 -0700

    update

---
+++ .cask/26.1/elpa/intero-20181102.1054/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20181102.1054" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "1a36b9b1f96840c449ade2aa4a3f2e8842a18892" :keywords '("haskell" "tools") :authors '(("Chris Done" . "chrisdone@fpcomplete.com")) :maintainer '("Chris Done" . "chrisdone@fpcomplete.com") :url "https://github.com/commercialhaskell/intero")
---
+++ .cask/26.1/elpa/org-clock-convenience-20160830.1856/org-clock-convenience-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-clock-convenience" "20160830.1856" "convenience functions for org time tracking" '((cl-lib "0.5") (org "8") (emacs "24.3")) :commit "2d3fab0991ef7fa8d94c46a63a66abd289c79d9e" :keywords '("org") :authors '(("Derek Feichtinger <dfeich.gmail.com>")) :maintainer '("Derek Feichtinger <dfeich.gmail.com>") :url "https://github.com/dfeich/org-clock-convenience")
