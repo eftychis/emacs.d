6784ba87cb4a5441c52b8818f52e270ed39a667f
Eftychios Theoodrakis <gildar@Eftychioss-MacBook-Pro.local>
2025-07-31 16:10:10 -0700

    Add elpaca for package management; load Cask with elpaca

--- init.el
+++ init.el
@@ -6,6 +6,65 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("melpa" . "http://melpa.org/packages/")))
+;;; Let us install elpaca. First we ned to install the version.
+(defvar elpaca-installer-version 0.11)
+;;; We need an elpaca directory to place everything under.
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+
+(defun elpaca-install-from-cask (cask-file)
+  "Read a CASK-FILE and install packages with Elpaca."
+  (with-temp-buffer
+    (insert-file-contents cask-file)
+    (goto-char (point-min))
+    (while (re-search-forward "(depends-on \\(\"\\([^\"]+\\)\"\\))" nil t)
+      (let ((pkg (match-string 2)))
+        (message "Installing %s via Elpaca..." pkg)
+        (eval `(elpaca ,(intern pkg)))))))
+
+;; Example usage
+(elpaca-install-from-cask "~/.emacs.d/Cask")
+
+
+
+(elpaca-wait)
+
 ;;
 (setq sage-shell:sage-root "/Applications/Sage-6.5.app/Contents/MacOS/Sage")
 
